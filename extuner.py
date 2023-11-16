fd29968cbfa5d39506d570fe2d659d24495fcd7e
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-16 17:11:10 +0800

    add main func

---
+++ extuner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*- 
+# cython:language_level=3
+
+from common.log import Logger
+
+# main function
+# if __name__=='__main__': 
+def main():
+     Logger().info("hello world")
