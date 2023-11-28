74ce3982102b9d59ec1f01d1ee45709e2ec270ba
zhanghan2021 <zhanghan@kylinos.cn>
2023-11-28 10:03:01 +0800

    Used for configuring and parsing command-line parameters, supporting multiple subcommands

---
+++ common/tool_cmd.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# cython:language_level=3
+
+import argparse
+import datetime
+import sys
+import platform
+
+from common.command import Command
+
+class ToolCmd:
+        
+    def __get_version(self):
+        # line = 'extuner version is 1.0. ' + 'Expires at ' + str(self.__last_day)
+        line = 'Extuner version is 1.0. ' 
+        return line
+
+    def args_help(self):
+        #共享可选参数wpath ipath opath
+        parent_parser = argparse.ArgumentParser(add_help=False)
+        parent_parser.add_argument('--work_path', type = str, default = '.', 
+                                   help=argparse.SUPPRESS)
+                                   #help = 'User can specify the conf path. default is /usr/share/extuner/conf')
+        parent_parser.add_argument('--inst_path', type = str, default = '.', 
+                                   help = argparse.SUPPRESS)
+        parent_parser.add_argument('-o','--out_path', type = str, default = '.', 
+                                   help = 'include log and report,  default is current directory')
+        #共享可选参数version
+        vparent_parser = argparse.ArgumentParser(add_help=False)
+        vparent_parser.add_argument('-v','--version' , action = 'version', version = self.__get_version(), 
+                                    help = 'version')
+        
+        #共享可选参数cust_col  收集脚本
+        cparent_parser = argparse.ArgumentParser(add_help=False)
+        cparent_parser.add_argument('--add ',   help = 'Extra collection data')
+      
+        #extuner命令
+        parser = argparse.ArgumentParser(
+            description='''description:\n  extuner is an expert tuning tool for Kylin system''',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            prog='extuner',
+            parents=[vparent_parser],
+            usage=''' \n  extuner COMMAND [options] ''',
+            epilog='''examples:\n    extuner collection --perf --pid -1''')
+        
+        subparsers = parser.add_subparsers(title = 'commands',prog='extuner',metavar=' ')
+        #子命令extuner collection
+        desc_c = 'collect the system data ,such as CPU/MEM/NET/IO'
+        parser_col = subparsers.add_parser('collection',
+            parents=[parent_parser], 
+            description='''description:\n  {}'''.format(desc_c),
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            help='* {}'.format(desc_c))
+
+        parser_col.add_argument('--func', type = str, default = 'col', 
+                                   help = argparse.SUPPRESS)      
+                
+        args = parser.parse_args()
+       
+        return args
+    
+    
