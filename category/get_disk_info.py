6d88e9322fdb6d2641e07787d78c2f910b4b9314
wuzhaomin <wuzhaomin@kylinos.cn>
2024-02-27 15:02:16 +0800

    Add the get fdisk function to the Diskinfo class

--- category/get_disk_info.py
+++ category/get_disk_info.py
@@ -4,9 +4,25 @@
 
 import os
 from common.file import FileOperation
+from common.command import Command
 
 class DiskInfo:
     def __init__(self, t_fileName):
         self.__default_file_name = t_fileName
         self.__bt_devlst = []
         FileOperation.remove_txt_file(self.__default_file_name)
+
+    def __get_fdisk_info(self):
+        '''
+            fdisk -l and blkid
+        '''
+        fdisk_command="fdisk -l"
+        cmd_name = fdisk_command 
+        cmd_result = Command.cmd_run(fdisk_command)
+        res_fdisk = FileOperation.wrap_output_format(cmd_name, cmd_result, '-')
+        blkid_command="blkid"
+        cmd_result = Command.cmd_run(blkid_command)
+        res_blkid = FileOperation.wrap_output_format(cmd_name, cmd_result, '=')
+        
+        res_all = res_fdisk + res_blkid
+        return Command.cmd_write_file(res_all, self.__default_file_name)
