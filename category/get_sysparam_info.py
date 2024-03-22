e5862a8d81d5a7db094199f45b68d53b81039602
lixiaoyong1 <lixiaoyong@kylinos.cn>
2024-03-22 17:39:50 +0800

    Add class SysParamInfo to file get_sysparam_info.py

--- category/get_sysparam_info.py
+++ category/get_sysparam_info.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # cython:language_level=3
+
+from common.file import FileOperation
+
+class SysParamInfo:
+    '''
+        System parameter class
+    '''
+    def __init__(self, t_fileName):
+        self.__default_file_name = t_fileName
