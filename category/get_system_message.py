d7cb53b688729e342deb0460b9d1e8197efd15d2
wangxiaomeng <wangxiaomeng@kylinos.cn>
2024-06-12 10:48:40 +0800

    add get_system_message.py file

---
+++ category/get_system_message.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# cython:language_level=3
+# Copyright (c) 2023 KylinSoft  Co., Ltd. All Rights Reserved.
+
+# System log class
+class SysMessage():
+    def __init__(self, t_fileName):
+        self.__default_file_name = t_fileName
