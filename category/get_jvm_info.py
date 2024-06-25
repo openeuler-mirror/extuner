4187f8373af33f2a3f6741d3c0cd1a93c0ef611e
李璐 <lilu@kylinos.cn>
2024-06-25 18:23:14 +0800

    add JVMInfo class and init function to get_jvm_info.py

--- category/get_jvm_info.py
+++ category/get_jvm_info.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # cython:language_level=3
+
+from common.file import FileOperation
+
+# jvm class
+class JVMInfo:
+    def __init__(self, t_fileName):
+        # output file
+        self.__default_file_name = t_fileName
+        FileOperation.remove_txt_file(self.__default_file_name)
