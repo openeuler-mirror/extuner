15e25019ef54048eafd126e6b8f00a0b0eb2544d
李璐 <lilu@kylinos.cn>
2024-07-09 10:08:01 +0800

    Add get_heap_usage_info function to get_jvm_info.py

--- category/get_jvm_info.py
+++ category/get_jvm_info.py
@@ -3,6 +3,7 @@
 # cython:language_level=3
 
 from common.file import FileOperation
+from common.command import Command
 
 # jvm class
 class JVMInfo:
@@ -10,3 +11,12 @@ class JVMInfo:
         # output file
         self.__default_file_name = t_fileName
         FileOperation.remove_txt_file(self.__default_file_name)
+        
+        
+    def __get_heap_usage_info(self):
+        '''
+            Get the java heap usage
+        '''
+        devices_command="jmap -heap {}".format(self.__pid)
+        cmd_result = Command.cmd_run(devices_command)
+        return Command.cmd_output("jmap -heap", cmd_result, self.__default_file_name, '=')
