684fc94785cba1915eeaf63d6f2f5fc4c751c40d
dongjiao <dongjiao@kylinos.cn>
2024-07-17 10:33:29 +0800

    add sysmessage func

--- summary_info.py
+++ summary_info.py
@@ -7,6 +7,7 @@ from category.get_net_info import NetInfo
 from category.get_cpu_info import CPUInfo
 from category.get_disk_info import DiskInfo
 from category.get_sysparam_info import SysParamInfo 
+from category.get_system_message import SysMessage
 from common.config import Config
 from common.log import Logger
 from common.global_call import GlobalCall
@@ -45,6 +46,9 @@ class SummaryInfo:
 
             SysParamInfo(GlobalCall.output_sys_param_file).get_info()
             Logger().info(u"系统参数采集完成")        
+
+            SysMessage(GlobalCall.output_sys_message_file).get_info()
+            Logger().info(u"系统日志采集完成")
             
             return True
         else:
